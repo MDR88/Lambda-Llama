eff0ef5ee31c8564170036ae201fa42c13a12ceb
mdroberts74 <mikedroberts88@gmail.com>
2018-06-22 15:41:00 -0500

    My first commit.

---
+++ main.js
@@ -0,0 +1,15 @@
+// Lambda Llama
+const llamaNamer = function() {
+    const possibleNames = ["Larry", "Leon", "Leona", "Les", "Laura", "Lemony", "Lars", "Lekisha"]
+    const randomizer = Math.floor(Math.random() * 7)
+
+    const namer = function() {
+        const suffix = " the Llama"
+        const name = possibleNames[randomizer]
+    }
+
+    return name + suffix
+}
+
+nameMaker = llamaNamer()
+console.log(nameMaker())
